e80706b777a1fbe91ee4e6e1f68efdfc9cd94351
WiliamMeloMota <102825885+WiliamMeloMota@users.noreply.github.com>
2022-09-05 19:50:38 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import NotificationButt from './components/NotificationButton'
+import Header from './components/Header'
+import SalesCard from './components/SalesCard'
 
 function App() {
   return (
     <>
-      <NotificationButt></NotificationButt>
+      <Header />
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard></SalesCard>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Heder() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={Logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Heder
